1a5beca69ba3608d9d6720a14e86887953645bf9
drbean <drbean@freeshell.org>
2016-08-02 23:22:21 +0900

    check_action_defined.if_not_select_*.to_test_db_access

--- lib/DB/Command/moodle.pm
+++ lib/DB/Command/moodle.pm
@@ -1,6 +1,6 @@
 package DB::Command::moodle;
 
-# Last Edit: 2016 Jul 10, 11:39:19 AM
+# Last Edit: 2016 Aug 02, 10:58:15 PM
 # $Id: /cloze/branches/ctest/dic.pl 1134 2007-03-17T11:05:37.500624Z greg  $
 
 use strict;
@@ -23,6 +23,7 @@ sub opt_spec  {
 use IO::All;
 use YAML qw/LoadFile/;
 use DB::Schema;
+use Encode;
 
 sub execute {
 	my ($self, $opt, $args) = @_;
@@ -36,11 +37,27 @@ sub execute {
 		, $opt->{u}
 		); 
 
-	if ( $opt->{a} eq "create") {
+	if ( $opt->{a} and $opt->{a} eq "create") {
 		my $class = $schema->resultset($opt->{t})->update_or_create($_) for @$data;
 	}
-	elsif ( $opt->{a} eq "delete" ) {
+	elsif ( $opt->{a} and $opt->{a} eq "delete" ) {
 		$schema->resultset($opt->{t})->delete;
+	}
+	else {
+		my @show;
+		my $rows = $schema->resultset($opt->{t});
+		my @columns = $rows->result_source->columns;
+		my $top_row; $top_row .= "$_\t" for @columns;
+		io('-')->print(  $top_row . "\n" );
+		while (my $row = $rows->next) {
+			my $show_row;
+			for my $column ( @columns ) {
+				if ( defined $row->$column  ) {
+					$show_row .= $row->$column . "\t";
+				}
+			}
+			io('-')->print(  Encode::encode( "UTF-8", "$show_row\n" ) );
+		}
 	}
 }
 
