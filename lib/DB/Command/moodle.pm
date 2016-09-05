cc3125fec2886d2bb9a0f3a8bc8801196defd2c4
drbean <drbean@freeshell.org>
2016-09-05 15:58:01 +0800

    delete:column,row

--- lib/DB/Command/moodle.pm
+++ lib/DB/Command/moodle.pm
@@ -1,13 +1,13 @@
 package DB::Command::moodle;
 
-# Last Edit: 2016 Aug 02, 10:58:15 PM
+# Last Edit: 2016 Sep 05, 03:57:32 PM
 # $Id: /cloze/branches/ctest/dic.pl 1134 2007-03-17T11:05:37.500624Z greg  $
 
 use strict;
 use warnings;
 
 use DB -command;
-sub usage_desc { "db moodle -p PORT,ie,cluster -u USER -d DATABASE -t TABLE -f FILE -a ACTION" }
+sub usage_desc { "db moodle -p PORT,ie,cluster -u USER -d DATABASE -t TABLE -f FILE -a ACTION [-c COLUMN -r ROW]"}
 
 sub opt_spec  {
 	return (
@@ -17,47 +17,55 @@ sub opt_spec  {
 		, ["t=s", "table"]
 		, ["f=s", "file"]
 		, ["a=s", "action"]
+		, ["c=s", "column"]
+		, ["r=s", "row"]
 	);
 }
 
 use IO::All;
 use YAML qw/LoadFile/;
 use DB::Schema;
-use Encode;
 
 sub execute {
 	my ($self, $opt, $args) = @_;
 
 	my $data;
 	$data = LoadFile
-		"/home/drbean/moodle/populate/" . $opt->{f} . ".yaml" if $opt->{f};
+	Whew, 	"/var/lib/moodle/populate/" . $opt->{f} . ".yaml" if $opt->{f};
 
 	my $schema = DB::Schema->connect(
 		"dbi:Pg:dbname=$opt->{d};port=$opt->{p}"
 		, $opt->{u}
 		); 
 
-	if ( $opt->{a} and $opt->{a} eq "create") {
+	if ( $opt->{a} eq "create") {
 		my $class = $schema->resultset($opt->{t})->update_or_create($_) for @$data;
 	}
-	elsif ( $opt->{a} and $opt->{a} eq "delete" ) {
-		$schema->resultset($opt->{t})->delete;
-	}
-	else {
-		my @show;
-		my $rows = $schema->resultset($opt->{t});
-		my @columns = $rows->result_source->columns;
-		my $top_row; $top_row .= "$_\t" for @columns;
-		io('-')->print(  $top_row . "\n" );
-		while (my $row = $rows->next) {
-			my $show_row;
-			for my $column ( @columns ) {
-				if ( defined $row->$column  ) {
-					$show_row .= $row->$column . "\t";
-				}
-			}
-			io('-')->print(  Encode::encode( "UTF-8", "$show_row\n" ) );
+	if ( $opt->{a} eq "delete" ) {
+		my $all_rows = $schema->resultset($opt->{t});
+		if ( $opt->{r} and $opt->{r} eq "all" ) {
+			$all_rows->delete;
 		}
+		elsif ( $opt->{c} and $opt->{r} ) {
+			my $some_rows = $all_rows->search({ $opt->{c} => $opt->{r} });
+			$some_rows->delete();
+		}
+		else {
+			die "-r 'all' or -c 'column' WHERE -r 'row_value'.\n"}
+
+	}
+	if ( $opt->{a} eq "select" ) {
+		my $all_rows = $schema->resultset($opt->{t});
+		if ( $opt->{r} and $opt->{r} eq "all" ) {
+			io('-')->print($all_rows);
+		}
+		elsif ( $opt->{c} and $opt->{r} ) {
+			my $some_rows = $all_rows->search({ $opt->{c} => $opt->{r} });
+			io('-')->print($some_rows);
+		}
+		else {
+			die "-r 'all' or -c 'column' WHERE -r 'row_value'.\n"}
+
 	}
 }
 
